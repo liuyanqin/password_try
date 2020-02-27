cd3ead84ee88afa7e31d8cf0049254867d0e546b
liuyanqin <953121426@qq.com>
2020-02-27 14:07:47 +0800

    Add new file

--- password.py
+++ password.py
@@ -1,16 +1,13 @@
-cs = 0  #次数
-while cs < 3:
+cs = 3  #次数
+while cs > 0:
 	password = input('请输入密码：')
-	cs = cs + 1
-	if password =='a123456':  #密码正确
+	cs = cs - 1
+	if password =='a123456':
 		print('登录成功')
 		break
-	else:   #密码错误
-		if  cs == 1:
-			print('密码错误，您还有两次机会')
-		elif  cs == 2:
-			print('密码错误，您还有一次机会')
-		else:
-			print('密码错误，请改天再试')
+	elif cs > 0: #3-cs>0
+		print('密码错误，您还有', cs, '次机会')
+	else:
+		print('密码错误，您已经没有机会了，请您改天再试试吧')
 
 		
